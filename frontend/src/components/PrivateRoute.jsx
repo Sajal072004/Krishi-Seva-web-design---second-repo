40b9c87fd43a2e6df506be26db0c2134ed7677b5
PrathamDwivedi27 <prathamd549@gmail.com>
2024-12-17 00:46:27 +0530

    Update reaadme

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ element, ...rest }) => {
   const token = localStorage.getItem('token');
 
-  return token ? element : <Navigate to="/sign-in" />;
+  return token ?<Navigate to="/sign-in" />: element  ;
 };
 
 export default PrivateRoute;
